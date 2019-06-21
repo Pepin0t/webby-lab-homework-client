9f2b2ea4a4c829a49bfa5a76d6934704edd93955
Pepinot <skripka.s.1988@gmail.con>
2019-06-21 16:01:18 +0300

    add timeouts

--- src/services/db.js
+++ src/services/db.js
@@ -1,13 +1,76 @@
 import axios from "axios";
 
-export const getListOfFilms = () => {
-    return axios.get("/api/get-list-of-films");
+export const getListOfFilms = async () => {
+    try {
+        const res = await axios({
+            method: "get",
+            url: "/api/get-list-of-films",
+            timeout: 5000
+        });
+
+        return Promise.resolve(res);
+    } catch (error) {
+        if (error.message === "timeout of 5000ms exceeded") {
+            return Promise.reject({
+                response: {
+                    data: {
+                        ok: false
+                    }
+                }
+            });
+        }
+
+        return Promise.reject(error);
+    }
 };
 
-export const addFilm = data => {
-    return axios.post("/api/add-film", { data });
+export const addFilm = async data => {
+    try {
+        const res = await axios({
+            method: "post",
+            url: "/api/add-film",
+            timeout: 5000,
+            data: { data }
+        });
+
+        return Promise.resolve(res);
+    } catch (error) {
+        if (error.message === "timeout of 5000ms exceeded") {
+            return Promise.reject({
+                response: {
+                    data: {
+                        ok: false,
+                        message: "Error. Try again!"
+                    }
+                }
+            });
+        }
+
+        return Promise.reject(error);
+    }
 };
 
-export const deleteFilm = ID => {
-    return axios.delete("/api/delete-film", { data: { ID } });
+export const deleteFilm = async ID => {
+    try {
+        const res = await axios({
+            method: "delete",
+            url: "/api/delete-film",
+            timeout: 5000,
+            data: { ID }
+        });
+
+        return Promise.resolve(res);
+    } catch (error) {
+        if (error.message === "timeout of 5000ms exceeded") {
+            return Promise.reject({
+                response: {
+                    data: {
+                        ok: false
+                    }
+                }
+            });
+        }
+
+        return Promise.reject(error);
+    }
 };
