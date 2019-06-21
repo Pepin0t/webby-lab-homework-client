0489ca85434b1fd72515b7c88b02dd4ec5fa2a05
Pepinot <skripka.s.1988@gmail.con>
2019-06-21 15:53:07 +0300

    split search and add search menu

--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -1,7 +1,8 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, createRef } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import debounce from "lodash/debounce";
+import classNames from "classnames";
 
 import { showNewFlimModal } from "../actions/modal_actions";
 import { sortListOfFilms, searchInListOfFilms } from "../actions/films_actions";
@@ -13,6 +14,17 @@ export class UserInterface extends PureComponent {
         search: PropTypes.func
     };
 
+    constructor(props) {
+        super(props);
+
+        this.input = createRef();
+
+        this.state = {
+            currentSearchCategory: "movies",
+            showSearchCategoriesMenu: false
+        };
+    }
+
     showNewFilmModal = () => {
         this.props.show();
     };
@@ -21,27 +33,106 @@ export class UserInterface extends PureComponent {
         this.props.sort();
     };
 
-    inputChange = e => {
-        return this.debouncedEvent(e.target.value);
+    inputChange = (e, value) => {
+        const inputValue = value || e.target.value;
+
+        return this.debouncedEvent(inputValue);
     };
 
     debouncedEvent = debounce(value => {
-        this.props.search(value);
+        this.props.search(value, this.state.currentSearchCategory);
     }, 300);
 
+    searchCategoriesMenuToggle = () => {
+        this.setState(
+            {
+                showSearchCategoriesMenu: !this.state.showSearchCategoriesMenu
+            },
+            () => {
+                if (!this.state.showSearchCategoriesMenu) {
+                    this.input.current.focus();
+                }
+            }
+        );
+    };
+
+    chooseSearchCategory = e => {
+        e.persist();
+
+        this.setState(
+            {
+                currentSearchCategory: e.target.name
+            },
+            () => {
+                const inputValue = this.input.current.value;
+
+                if (inputValue) {
+                    this.inputChange(null, inputValue);
+                }
+            }
+        );
+    };
+
     render() {
+        const ifSearchMenuClass = classNames({
+            "on-front": this.state.showSearchCategoriesMenu
+        });
+
         return (
             <div id="user-interface">
-                <button id="show-modal" onClick={this.showNewFilmModal}>
+                <button id="show-modal-button" onClick={this.showNewFilmModal}>
                     Add new film
                 </button>
-                <button id="sort-list" onClick={this.sortListOfFilms}>
+                <button id="sort-list-button" onClick={this.sortListOfFilms}>
                     Sort list
                 </button>
-                <label htmlFor="text-input">
-                    Search
-                    <input id="text-input" type="text" name="search" onChange={this.inputChange} />
-                </label>
+                <div id="search-wrapper">
+                    <label htmlFor="text-input">
+                        Search
+                        <input
+                            ref={this.input}
+                            id="text-input"
+                            className={ifSearchMenuClass}
+                            type="text"
+                            name="search"
+                            placeholder={this.state.currentSearchCategory}
+                            onChange={this.inputChange}
+                            disabled={this.state.showSearchCategoriesMenu}
+                            autoFocus
+                        />
+                    </label>
+                    <button
+                        id="change-search-category-button"
+                        className={ifSearchMenuClass}
+                        onClick={this.searchCategoriesMenuToggle}
+                    >
+                        {this.state.showSearchCategoriesMenu ? <>&times;</> : <>&#x25bc;</>}
+                    </button>
+
+                    {this.state.showSearchCategoriesMenu && (
+                        <>
+                            <div id="behind-categories-menu" />
+                            <div id="search-categories">
+                                <button
+                                    className="category"
+                                    name="movies"
+                                    disabled={this.state.currentSearchCategory === "movies"}
+                                    onClick={this.chooseSearchCategory}
+                                >
+                                    Movies
+                                </button>
+                                <button
+                                    className="category"
+                                    name="stars"
+                                    disabled={this.state.currentSearchCategory === "stars"}
+                                    onClick={this.chooseSearchCategory}
+                                >
+                                    Stars
+                                </button>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         );
     }
